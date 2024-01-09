f2575108e7e54f96fab897fca0f96068c53e9934
Ben Cohen <codebrojects@gmail.com>
2024-01-09 20:38:17 +0200

    added files for microservicetwo

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const axios = require('axios')
+const app = express()
+const port = 3002
+
+const makeRequest = async (req, res) =>{
+    const response = await axios.get('http://localhost:3000/microservicetwoname?lastname=bakal');
+    console.log(response.data)
+}
+makeRequest();
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
