09de0a037ad237ef1db00d2c32f4ca056e55629b
Ben Cohen <codebrojects@gmail.com>
2024-01-16 21:16:49 +0200

    added request from user

--- app.js
+++ app.js
@@ -7,21 +7,8 @@ const makeRequest = async (req, res) =>{
     const response = await axios.get('http://localhost:3000/microservicetwoname?lastname=bakal');
     console.log(response.data)
 }
-requestFromUser();
-const requestFromUser = () =>{
-  let choice  = '';
-  console.log("Invoke request ? Y/N ");
-  prompt(choice);
-  switch(choice){
-    case 'Y':
-      makeRequest();
-      break;
-      default:
-        console.log("thank you have a nice day.");
-        break;
-      }
-      return;
-}
+makeRequest();
+  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
