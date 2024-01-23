e8e875b42252d5492dabdf34f3035bcc397df277
Omribak <97708552+Omribak@users.noreply.github.com>
2024-01-23 10:49:44 +0200

    New AWS url

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const express = require('express')
-const axios = require('axios')
-const app = express()
-const port = 3002
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const port = 3002;
 
-const makeRequest = async (req, res) =>{
-    const response = await axios.get('http://localhost:3000/microservicetwoname?lastname=bakal');
-    console.log(response.data)
-}
+const makeRequest = async (req, res) => {
+  const response = await axios.get(
+    "http://107.22.38.115/microservicetwoname?lastname=bakal"
+  );
+  console.log(response.data);
+};
 makeRequest();
-  
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example Micro Service 2 listening on port ${port}`);
+});
